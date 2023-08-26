b184087ff47fef8d0b8a655148c36b616aaa09ec
Dhananjaya Kuppu <dhananjayak@yahoo.com>
2023-08-26 05:42:50 +0000

    Finished The Simple Bots

--- simplebot.py
+++ simplebot.py
@@ -8,6 +8,25 @@ import os
 import openai
 openai.api_key=os.getenv('OPENAI_KEY')
 
+# Create the main application window using Tk()
+baseWindow = Tk()
+
+# Set the title of the window
+baseWindow.title("The Simple Bot")
+
+# Set the size of the window
+baseWindow.geometry("500x250")
+
+# Create the chat window as a ScrolledText widget with "Arial" font
+chatWindow = tks.ScrolledText(baseWindow, font="Arial")
+
+# Configure tags for message alignment: 'tag-left' for bot messages, 'tag-right' for user messages
+chatWindow.tag_configure('tag-left', justify='left')
+chatWindow.tag_configure('tag-right', justify='right')
+
+# Disable the chat window initially (it should not be editable by the user)
+chatWindow.config(state=DISABLED)
+
 def send(event):
     chatWindow.config(state=NORMAL)
 
@@ -62,7 +81,7 @@ def create_and_insert_bot_frame(bot_response):
   chatWindow.window_create('end', window=botFrame)
   chatWindow.insert(END, "\n\n" + "")
 
-  
+
 # Generating response
 
 # Generating response
@@ -81,24 +100,6 @@ def get_bot_response(user_input):
     return bot_response
 
 
-# Create the main application window using Tk()
-baseWindow = Tk()
-
-# Set the title of the window
-baseWindow.title("The Simple Bot")
-
-# Set the size of the window
-baseWindow.geometry("500x250")
-
-# Create the chat window as a ScrolledText widget with "Arial" font
-chatWindow = tks.ScrolledText(baseWindow, font="Arial")
-
-# Configure tags for message alignment: 'tag-left' for bot messages, 'tag-right' for user messages
-chatWindow.tag_configure('tag-left', justify='left')
-chatWindow.tag_configure('tag-right', justify='right')
-
-# Disable the chat window initially (it should not be editable by the user)
-chatWindow.config(state=DISABLED)
 
 # Create the send button, with specific font, text, and background color
 # The 'command' option is commented out. Uncomment it and replace 'send' with your send function's name
@@ -110,6 +111,7 @@ sendButton = Button(
     activebackground="#ff467e",
     fg='#ffffff',
     command=send)
+    
 sendButton.bind("<Button-1>", send)
 baseWindow.bind('<Return>', send)
 
@@ -123,5 +125,3 @@ sendButton.place(x=430, y=200)
 
 # Start the main event loop to keep the application running and responsive
 baseWindow.mainloop()    
-
-
